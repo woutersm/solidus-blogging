e5b71e918847004d047b9f26e8c9b2463fcdb59a
David Demers <ddemers@etelephonesystems.com>
2011-02-19 22:19:04 -0500

    remove before filters and added object method on blog entries controller to fix show authorization

--- app/controllers/blog_entries_controller.rb
+++ app/controllers/blog_entries_controller.rb
@@ -2,18 +2,14 @@ class BlogEntriesController < Spree::BaseController
   resource_controller
   actions :show, :index
 
-  before_filter :load_news_archive_data
-
-  index.before do
-    @blog_entries = BlogEntry.find :all
-  end
-
-  show.before do
-    @blog_entry = BlogEntry.find_by_permalink(params[:slug])
+  before_filter :load_news_archive_data   
+  
+  def object
+    @object ||= end_of_association_chain.find_by_permalink(params[:slug])
   end
 
   def tag
-    @blog_entries = BlogEntry.by_tag(params[:tag])
+    @blog_entries = BlogEntry.by_tag(params[:tag])   
     render :action => :index
   end
 
