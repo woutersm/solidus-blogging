8cdb05e54fa5051f5031afe4ecc3253658d25192
David Demers <fonemstr@gmail.com>
2012-07-25 19:09:52 -0400

    fix namespace for admin controller

--- app/controllers/spree/admin/blog_entries_controller.rb
+++ app/controllers/spree/admin/blog_entries_controller.rb
@@ -1,4 +1,4 @@
-class Spree::Admin::BlogEntriesController < Admin::ResourceController
+class Spree::Admin::BlogEntriesController < Spree::Admin::ResourceController
   #resource_controller
 
   # index.before do 
