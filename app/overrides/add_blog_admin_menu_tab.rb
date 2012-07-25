d18b0351dbf356f7e59f2d8715a1cf65136d4cb5
David Demers <fonemstr@gmail.com>
2012-07-25 19:18:39 -0400

    fix override for blog admin tab

--- app/overrides/add_blog_admin_menu_tab.rb
+++ app/overrides/add_blog_admin_menu_tab.rb
@@ -1,4 +1,4 @@
-Deface::Override.new(:virtual_path => "layouts/admin",
+Deface::Override.new(:virtual_path => "spree/layouts/admin",
                      :name => "add_blog_admin_menu",
                      :insert_bottom => "[data-hook='admin_tabs'], #admin_tabs[data-hook]",
                      :text => "<%= tab(:blog_entries, :label => 'Blog') %>",
