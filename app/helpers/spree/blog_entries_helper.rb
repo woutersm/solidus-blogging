955fc17988407b7d51a8963ff2dd7ccbeeb7cd7d
Randy Terbush <randy@terbush.org>
2013-05-03 08:41:21 -0600

    Port to 2.0
    Fix up a problem where if no published_at date, display will fail.
    Not sure if this is the right solution. Confused by the existence of "published_at" and "visible"
    flag.

--- app/helpers/spree/blog_entries_helper.rb
+++ app/helpers/spree/blog_entries_helper.rb
@@ -5,10 +5,18 @@ module Spree
          BlogEntry.recent.collect { |post| link.new( post.title, blog_entry_permalink(post)) }
     end
     def blog_entry_permalink(e)
-       blog_entry_permalink_path :year => e.published_at.strftime("%Y"), :month => e.published_at.strftime("%m"), :day => e.published_at.strftime("%d"), :slug => e.permalink
+      unless e.published_at.nil?
+        blog_entry_permalink_path :year => e.published_at.strftime("%Y"), :month => e.published_at.strftime("%m"), :day => e.published_at.strftime("%d"), :slug => e.permalink
+      else
+        blog_entry_permalink_path :year => "na", :month => "na", :day => "na", :slug => e.permalink
+      end
     end
     def blog_entry_url_permalink(e)
-       blog_entry_permalink_url :year => e.published_at.strftime("%Y"), :month => e.published_at.strftime("%m"), :day => e.published_at.strftime("%d"), :slug => e.permalink
+      unless e.published_at.nil?
+         blog_entry_permalink_url :year => e.published_at.strftime("%Y"), :month => e.published_at.strftime("%m"), :day => e.published_at.strftime("%d"), :slug => e.permalink
+       else
+        blog_entry_permalink_url :year => "na", :month => "na", :day => "na", :slug => e.permalink
+      end
     end
 
     def blog_full_article_html(blog_entry)
