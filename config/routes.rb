35a3446929c0f541907c81f59bca2fb1e493edbc
Anton Aljoshin <Anton_Aleschin@mail.ru>
2014-02-03 18:59:47 +0400

    Refactor routes in Rails 4 way.

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,11 @@ Spree::Core::Engine.routes.draw do
   
   resources :blog_entries
 
-  match '/blog/tag/:tag', :to => 'blog_entries#tag', :as => :blog_tag
-  match '/blog/category/:category', :to => 'blog_entries#category', :as => :blog_category
-  match '/blog/author/:author', :to => 'blog_entries#author', :as => :blog_author
-  match '/blog/:year/:month/:day/:slug', :to => 'blog_entries#show', :as => :blog_entry_permalink
-  match '/blog/:year(/:month)(/:day)', :to => 'blog_entries#archive', :as => :news_archive,
-    :constraints => {:year => /(19|20)\d{2}/, :month => /[01]?\d/, :day => /[0-3]?\d/}
-  match '/blog/feed', :to => 'blog_entries#feed', :as => :blog_feed, :format => :rss
-  match '/blog', :to => 'blog_entries#index', :as => :blog
+  get '/blog/tag/:tag' => 'blog_entries#tag', :as => :blog_tag
+  get '/blog/category/:category' => 'blog_entries#category', :as => :blog_category
+  get '/blog/author/:author' => 'blog_entries#author', :as => :blog_author
+  get '/blog/:year/:month/:day/:slug' => 'blog_entries#show', :as => :blog_entry_permalink
+  get '/blog/:year(/:month)(/:day)' => 'blog_entries#archive', :as => :news_archive, :constraints => {:year => /(19|20)\d{2}/, :month => /[01]?\d/, :day => /[0-3]?\d/}
+  get '/blog/feed' => 'blog_entries#feed', :as => :blog_feed, :format => :rss
+  get '/blog' => 'blog_entries#index', :as => :blog
 end
