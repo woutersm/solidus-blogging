a6fab8c21f0a3a9be3b310778fc5a5fcf440f433
Hugo Santos <hugo.santos@factorlibre.com>
2014-02-10 18:47:16 +0100

    Fix Admin tab in menu

--- app/overrides/add_blog_admin_menu_tab.rb
+++ app/overrides/add_blog_admin_menu_tab.rb
@@ -1,5 +1,5 @@
-Deface::Override.new(:virtual_path => "spree/layouts/admin",
+Deface::Override.new(:virtual_path => "spree/admin/shared/_menu",
                      :name => "blog_admin_tabs",
-                     :insert_bottom => "[data-hook='admin_tabs'], #admin_tabs[data-hook]",
+                     :insert_bottom => "[data-hook='admin_tabs']",
                      :text => "<%= tab(:blog_entries, :label => 'Blog', :url => spree.admin_blog_entries_path, :icon => '') %>",
                      :disabled => false)
