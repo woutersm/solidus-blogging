8e36952b88ea3ec3ab1bbacac0abd18997e86a44
forever sumit <forever.sumitsharma@gmail.com>
2014-08-28 12:53:04 +0530

    changes in admin tab for blog

--- app/overrides/add_blog_admin_menu_tab.rb
+++ app/overrides/add_blog_admin_menu_tab.rb
@@ -1,5 +1,5 @@
 Deface::Override.new(:virtual_path => "spree/admin/shared/_menu",
                      :name => "blog_admin_tabs",
                      :insert_bottom => "[data-hook='admin_tabs'], #admin_tabs[data-hook]",
-                     :text => "<%= tab(:blog_entries, :label => 'Blog', :url => spree.admin_blog_entries_path, :icon => 'icon-file') if can? :manage, Spree::BlogEntry %>",
+                     :text => "<%= tab(:blog_entries, :label => 'Blog', :url => spree.admin_blog_entries_path, :icon => 'file') if can? :manage, Spree::BlogEntry %>",
                      :disabled => false)
